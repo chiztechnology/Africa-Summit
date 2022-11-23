76c702ccb1658939a9cb9801be17d78eaf3b23a5
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-11-23 22:00:33 +0200

    Clear code with linters

--- js/main.js
+++ js/main.js
@@ -25,32 +25,32 @@ const speakers = [
     name: 'Israel CHIZUNGU',
     function: 'Data Analyst & Software Engineer',
     description: 'Data analysts gather and scrutinise data using specialist tools to generate information that helps others make decisions. They will respond to questions about data and look for trends, patterns and anomalies within it',
-    img: 'speaker_1.jpeg'
+    img: 'speaker_1.jpeg',
   },
   {
     id: 2,
     name: 'Gabriel ZINCH',
     function: 'Teacher and assistant',
     description: 'Develops, plans, and implements curriculum, lesson plans, and educational programs for student audiences within areas of expertise. Advises, tests, and teaches students audiences in a variety of academic subjects.',
-    img: 'speaker_2.webp'
+    img: 'speaker_2.webp',
   }, {
     id: 3,
     name: 'Kandi Bryan',
     function: 'Eonomist and Development Actor',
     description: 'responsible for analyzing market trends, advising businesses and governments on economic decisions, and developing econometric models for forecasting, among other duties.',
-    img: 'speaker_3.jpeg'
+    img: 'speaker_3.jpeg',
   }, {
     id: 1,
     name: 'Nina MAYOLA',
     function: 'Data Analyst & Software Engineer',
     description: 'Data analysts gather and scrutinise data using specialist tools to generate information that helps others make decisions. They will respond to questions about data and look for trends, patterns and anomalies within it',
-    img: 'speaker_4.jpeg'
-  }
+    img: 'speaker_4.jpeg',
+  },
 ];
 
 // document.onload = ()=>{
 speakers.forEach((speaker) => {
-  document.getElementById("speaker-articles-container").innerHTML += `<article class="speaker-article">
+  document.getElementById('speaker-articles-container').innerHTML += `<article class="speaker-article">
     <div class="speaker-article-img-container">
         <img class="speaker-article-img" src="img/${speaker.img}" alt="speaker image" />
     </div>
