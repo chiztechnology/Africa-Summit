794d1e6542cdb2e990d2126cbbba9ce83e23603f
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-11-23 19:12:56 +0200

    Fetch dynamic speakers from js object

--- js/main.js
+++ js/main.js
@@ -18,3 +18,49 @@ document.getElementById('home-link').addEventListener('click', () => {
 document.getElementById('about-link').addEventListener('click', () => {
   document.querySelector('.mobile-nav').setAttribute('style', 'visibility:hidden');
 });
+
+const speakers = [
+  {
+    id: 1,
+    name: 'Israel CHIZUNGU',
+    function: 'Data Analyst & Software Engineer',
+    description: 'Data analysts gather and scrutinise data using specialist tools to generate information that helps others make decisions. They will respond to questions about data and look for trends, patterns and anomalies within it',
+    img: 'speaker_1.jpeg'
+  },
+  {
+    id: 2,
+    name: 'Gabriel ZINCH',
+    function: 'Teacher and assistant',
+    description: 'Develops, plans, and implements curriculum, lesson plans, and educational programs for student audiences within areas of expertise. Advises, tests, and teaches students audiences in a variety of academic subjects.',
+    img: 'speaker_2.webp'
+  },{
+    id: 3,
+    name: 'Kandi Bryan',
+    function: 'Eonomist and Development Actor',
+    description: 'responsible for analyzing market trends, advising businesses and governments on economic decisions, and developing econometric models for forecasting, among other duties.',
+    img: 'speaker_3.jpeg'
+  },{
+    id: 1,
+    name: 'Nina MAYOLA',
+    function: 'Data Analyst & Software Engineer',
+    description: 'Data analysts gather and scrutinise data using specialist tools to generate information that helps others make decisions. They will respond to questions about data and look for trends, patterns and anomalies within it',
+    img: 'speaker_4.jpeg'
+  }
+];
+
+// document.onload = ()=>{
+  speakers.forEach((speaker) => {
+    document.getElementById("speaker-articles-container").innerHTML +=`<article class="speaker-article">
+    <div class="speaker-article-img-container">
+        <img class="speaker-article-img" src="img/${speaker.img}" alt="speaker image" />
+    </div>
+    <div class="speaker-article-description-container">
+        <h4 class="speaker-name">${speaker.name}</h4>
+        <h4 class="speaker-position">${speaker.function}</h4>
+        <p class="speaker-description">
+            ${speaker.description}
+        </p>
+    </div>
+</article>`;
+  });
+// };
